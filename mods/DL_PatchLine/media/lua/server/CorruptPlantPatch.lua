e138000cd3adddd0a54418429799b2b7ad477b83
Westen <40703891+Westen-271@users.noreply.github.com>
2025-09-26 12:07:57 +0100

    Fix plant harvesting by overriding getVegetablesNumber.

--- mods/DL_PatchLine/media/lua/server/CorruptPlantPatch.lua
+++ mods/DL_PatchLine/media/lua/server/CorruptPlantPatch.lua
@@ -40,3 +40,68 @@ function badPlant(water, waterMax, diseaseLvl, plant, nextGrowing, updateNbOfGro
         plant.nbOfGrow = plant.nbOfGrow -1;
     end
 end
+
+PatchLine_GetVegetablesNumberOriginal = _G["getVegetablesNumber"];
+
+_G["getVegetablesNumber"] = function(min, max, minAuthorised, maxAuthorised, plant)
+    -- Basic nil value prevention (there's a lot of invalid plant stats cropping up!)
+    if not plant then return 0 end;
+
+    if not plant.health then
+        plant.health = 0;
+    end;
+
+    -- Health modifier is: for every 10 HP points over 50, gain 1 more vegetable (base game).
+    -- This means you can gain up to 5 extra veg in a harvest if at good HP.
+    local healthModifier = math.floor((plant.health - 50) / 10);
+	if healthModifier < 0 then
+		healthModifier = 0;
+    end
+
+    -- Next is the server's difficulty modifier.
+    local difficultyModifier = 0;
+    if SandboxVars.PlantAbundance == 1 then -- Very Poor
+        difficultyModifier = -4;
+    elseif SandboxVars.PlantAbundance == 2 then -- Poor
+        difficultyModifier = -2;
+    elseif SandboxVars.PlantAbundance == 4 then -- Abundant
+        difficultyModifier = 3;
+    elseif SandboxVars.PlantAbundance == 5 then -- Very Abundant
+        difficultyModifier = 5;
+    end
+
+    local modifierBonus = healthModifier + difficultyModifier;
+    local minV = min + modifierBonus; -- The absolute minimum with modifiers.
+    local maxV = max + modifierBonus; -- As above but max.
+
+    local minAuthorisedModified = minAuthorised + modifierBonus;
+    local maxAuthorisedModified = maxAuthorised + modifierBonus;
+
+    -- Bring the minimum to within a range of 1 - minAuthorised+modifier unless it would take below 0.
+    -- This has changed from the base game code because it made no sense.
+    if minV > minAuthorisedModified and minAuthorisedModified > 0 then
+        minV = minAuthorisedModified;
+    end
+
+    if maxV > maxAuthorisedModified then
+        maxV = maxAuthorisedModified;
+    end
+
+    local nbOfVegetable = ZombRand(minV, maxV + 1);
+
+    if not plant.aphidLvl then
+        plant.aphidLvl = 0;
+    end
+
+    -- Lower harvest by 1 per every 10 aphid level.
+	local aphidModifier = math.floor(plant.aphidLvl / 10);
+	nbOfVegetable = nbOfVegetable - aphidModifier;
+
+    if nbOfVegetable < 1 and aphidModifier > 5 then -- This means if you don't treat the aphid level you will get 0 crops on an unlucky roll.
+        return 0;
+    elseif nbOfVegetable < 1 then
+        return 1;
+    else
+        return nbOfVegetable;
+    end
+end
