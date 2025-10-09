fd9a3f3bfe634af803f769355e7ef3e69a22608b
Westen <40703891+Westen-271@users.noreply.github.com>
2025-10-09 12:54:06 +0100

    ISRemoveGlass malformed packet / sync patch

---
+++ mods/DL_PatchLine/media/lua/client/ISRemoveGlassFix.lua
@@ -0,0 +1,28 @@
+local _ISRemoveGlassPerform_original = ISRemoveGlass.perform;
+
+function ISRemoveGlass:perform()
+    _ISRemoveGlassPerform_original(self);
+
+    sendClientCommand(self.character, "ISRemoveGlassPatch", "RemoveGlassFromBodyPart", { target = self.character, bodyPart = self.bodyPart });
+end
+
+local function ISRemoveGlassPatch_OnServerCommand(module, command, args)
+    if module ~= "ISRemoveGlassPatch" then return end;
+    if command ~= "RemoveGlassFromBodyPart" then return end;
+    if not args.bodyPart then return end;
+
+    local type = args.bodyPart:getType();
+    if not type then return end;
+
+    local clientPart = getPlayer():getBodyDamage():getBodyPart(type);
+    if not clientPart then
+        error("[ISRemoveGlass] Attempted to remove glass in body part " .. tostring(type) .. " but could not find it.");
+        return;
+    end
+
+    if clientPart:haveGlass() then
+        clientPart:setHaveGlass(false);
+    end
+end
+
+Events.OnServerCommand.Add(ISRemoveGlassPatch_OnServerCommand);
