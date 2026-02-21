fe3895cb5492e56050fc70f43919bbe9ab184fdc
Westen <40703891+Westen-271@users.noreply.github.com>
2026-02-21 08:12:57 +0000

    Fix edge case with duplicates and turn rolling

---
+++ mods/DL_DeadlineDonatorPicker/media/lua/server/LootTicket_ServerManager.lua
@@ -0,0 +1,126 @@
+if isClient() and not isServer() then return end;
+
+LootTicket_ServerManager = {};
+
+local randInstance = newrandom();
+
+function LootTicket_ServerManager.PerformTicketRoll(player, ticket, lootData)
+
+    if not player then
+        print("LootTicket_ServerManager.PerformTicketRoll - invalid target username provided, cannot find player.");
+        return;
+    end
+
+    local itemsToGive = {};
+
+    local totalChance = 0;
+    local logStr = "[OPENING LOOT TICKET] Player: " .. player:getUsername() .. " || ID: " .. tostring(lootData.ID) ..  " || ";
+
+
+    for _, itemData in ipairs(lootData.Items) do
+        local chance = tonumber(itemData.item.Chance);
+
+        itemData.item.hasRolledThisTurn = false;
+        if chance < 100 then
+            totalChance = totalChance + chance;
+        else
+            table.insert(itemsToGive, itemData);
+        end
+    end
+
+    logStr = logStr .. string.format(" || Total Weight: %0d ", totalChance);
+
+    local totalRolls = tonumber(lootData.MaxRolls);
+    if not totalRolls or totalRolls < 1 then totalRolls = 1 end;
+
+    logStr = logStr .. string.format(" || Total Rolls: %0d ", totalRolls);
+
+    for rollNum = 1, totalRolls do
+        randInstance:seed(getTimestampMs());
+        local iteratedChance = randInstance:random(0, totalChance);
+        local hasRolledSuccessfully = false;
+
+        for _, rollItem in ipairs(lootData).Items do
+            rollItem.item.hasRolledThisTurn = false;
+        end
+
+        logStr = logStr .. string.format(" || Current Roll: %0d, Random Number Picked: %0d >", rollNum, iteratedChance);
+
+        for _, rollItem in ipairs(lootData.Items) do
+            logStr = logStr .. string.format(" [%s ", rollItem.item.Name);
+            
+            -- If the item is guaranteed, add it to the list.
+            if tonumber(rollItem.item.Chance) < 100 then
+                -- Make sure there's not already been an item selected for this roll (not withstanding the guaranteed ones.)
+                if not hasRolledSuccessfully then
+
+                    iteratedChance = iteratedChance - tonumber(rollItem.item.Chance);
+
+                    logStr = logStr .. string.format("original: %0d, comp is: %0d", rollItem.item.Chance, iteratedChance);
+
+                    if not lootData.AllowDuplicates and not rollItem.item.hasRolledThisTurn then
+                        
+                        if iteratedChance <= 0 then
+                            logStr = logStr .. string.format(" !SUCCESS! ", iteratedChance);
+                            hasRolledSuccessfully = true;
+                            rollItem.item.hasRolledThisTurn = true;
+                            table.insert(itemsToGive, rollItem);
+                        end
+                    elseif lootData.allowDuplicates then
+                        logStr = logStr .. string.format(" !SUCCESS! ", iteratedChance);
+                        hasRolledSuccessfully = true;
+                        table.insert(itemsToGive, rollItem);
+                    else
+                        logStr = logStr .. " !Has already rolled, no duplicates allowed ! ";
+                    end
+                else
+                    logStr = logStr .. " skip";
+                end
+
+                logStr = logStr .. " ] ";
+            end
+
+            
+        end
+    end
+
+    writeLog("LootTicket", logStr);
+    print(logStr);
+
+    local rewardStr = "[REWARD] Giving player " .. player:getUsername() .. " the following items: ";
+    for _, item in ipairs(itemsToGive) do
+        player:sendObjectChange('addItemOfType', { type = item.item.Name, count = tonumber(item.item.Quantity) });
+        rewardStr = rewardStr .. item.item.Name .. " [count: " .. item.item.Quantity .. "] |";
+    end
+
+    player:sendObjectChange('removeItemID', { id = ticket:getID(), type = ticket:getFullType() });
+
+    writeLog("LootTicket", rewardStr);
+    print(rewardStr);
+
+    sendServerCommand(player, "LootTicket", "ReceiveRollResults", { success = true, ticket = ticket, rewards = itemsToGive });
+end
+
+function LootTicket_ServerManager.OnClientCommand(module, command, player, args)
+
+    if module ~= "LootTicket" then return end;
+
+    if command == "RequestRoll" then
+        if not args.ticket then
+            print("LootTicket_ServerManager - RequestRoll called with no ticket provided.");
+            return;
+        end
+
+        if not args.lootData then
+            print("LootTicket_ServerManager - RequestRoll called with no loot ticket data provided.");
+            return;
+        end
+
+        LootTicket_ServerManager.PerformTicketRoll(player, args.ticket, args.lootData);
+    end
+
+end
+
+Events.OnClientCommand.Add(LootTicket_ServerManager.OnClientCommand);
+
+return LootTicket_ServerManager;
