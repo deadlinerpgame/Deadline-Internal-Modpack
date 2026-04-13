d49905467e9459dd8cb923575612d53cc9cd111d
Westen <40703891+Westen-271@users.noreply.github.com>
2026-04-13 05:55:23 +0100

    Add gas mask tag check to radline

--- mods/DL_RadLine/media/lua/client/Radiation.lua
+++ mods/DL_RadLine/media/lua/client/Radiation.lua
@@ -90,6 +90,7 @@ function IsItemGasMask(item)
     if string.find(itemType, "PrepperMask") or string.find(itemType, "StalkerMask") then return true end;
     if string.find(itemType, "Respirator") and (not string.find(itemType, "Neck")) then return true end;
     if string.find(itemType, "NBCMask") then return true end;
+    if item:hasTag("GasMask") then return true end;
     return false;
 end
 
